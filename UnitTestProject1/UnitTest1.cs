6c5fcf6598c8433628932f7e8fc13e6a255e339d
Vasili <undertheoakstud@gmail.com>
2021-08-26 17:05:04 +0300

    Add to proj TESTS

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(@"e:\2");
+            Assert.IsTrue(dinfo.Exists);
+        }
+        public void TestMethod2()
+        {
+            Assert.IsNotNull(string.IsNullOrEmpty("1.txt"));
+        }
+        public void TestMethod3()
+        {
+            Assert.IsTrue(!File.Exists("1.txt"));
+        }
+        public void TestMethod4()
+        {
+            Assert.IsNotNull(File.OpenText("1.txt"));
+        }
+        public void TestMethod5()
+        {            
+            using (TextReader reader = File.OpenText("1.txt"))
+            {
+                string line = reader.ReadLine();
+                Assert.IsNotNull(line);
+            }
+        }
+
+
+    }
+}
